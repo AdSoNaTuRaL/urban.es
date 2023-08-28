c51e20183580fb196ffc38a5eaeb2a9d51f1982d
Adson Henrique <adsonhenriquesilva@gmail.com>
2023-08-28 21:01:24 +0200

    Added some rules and validation

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -1,8 +1,9 @@
 <?php
-
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreProductRequest extends FormRequest
 {
@@ -13,7 +14,7 @@ class StoreProductRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +25,23 @@ class StoreProductRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => 'required|string|max:100',
+            'description' => 'required|string|max:500',
+            'image' => 'required|url|max:500',
         ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name' => 'The name field is required.',
+            'description' => 'The description field is required.',
+            'image' => 'The image field is required.',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
     }
 }
